b70ac30741a9c398d0442c237f553c08113ba8e7
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-03-27 12:31:31 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2723,7 +2723,12 @@ class HomeController
 
 			$artify = DB::ArtifyCrud();
 			$queryfy = $artify->getQueryfyObj();
-			$queryfy->insert("tickets", array("funcionario" => $funcionario, "titulo" => $titulo, "contenido" => $contenido, "estado" => "Ingresado"));
+			$queryfy->insert("tickets", array(
+				"funcionario" => $funcionario, 
+				"titulo" => $titulo, 
+				"contenido" => $contenido, 
+				"estado" => "Ingresado"
+			));
 			echo json_encode(["mensaje" => "Ticket creado con éxito"]);
 		}
 	}
