561bffddbe0f81d6270b226c21003da5a4346307
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-04-07 08:47:04 -0400

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2547,7 +2547,20 @@ class HomeController
 	}
 
 	public function bot(){
-		$artify = DB::ArtifyCrud();
+
+		$settings["script_url"] = $_ENV['URL_ArtifyCrud'];
+        $_ENV["url_artify"] = "artify/artifycrud.php";
+        $settings["url_artify"] = $_ENV["url_artify"];
+        $settings["downloadURL"] = $_ENV['DOWNLOAD_URL'];
+        $settings["hostname"] = $_ENV['DB_HOST'];
+        $settings["database"] = $_ENV['DB_NAME'];
+        $settings["username"] = $_ENV['DB_USER'];
+        $settings["password"] = $_ENV['DB_PASS'];
+        $settings["dbtype"] = $_ENV['DB_TYPE'];
+        $settings["characterset"] = $_ENV["CHARACTER_SET"];
+
+        $autoSuggestion = true;
+        $artify = DB::ArtifyCrud(false, "", "", $autoSuggestion, $settings);
 		$artify->addPlugin("chosen");
 		$artify->fieldCssClass("user_message", array("frases"));
 		$artify->formFields(array("user_message"));
