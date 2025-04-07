0ebabf49b1efefa5a860e6e7855aff0e5f83055a
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-04-07 11:28:43 -0400

    up

--- app/Views/bot.php
+++ app/Views/bot.php
@@ -26,15 +26,6 @@
                                 <div class="chat-container mb-3">
                                     <div class="chat-header">Tickibot - Soporte en tiempo real</div>
 
-                                   <!--<div class="row">
-                                        <div class="col-md-10">
-                                            <input type="text" id="search-messages" class="form-control mt-2 mb-2" placeholder="Buscar mensajes...">
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button class="btn btn-primary btn-block buscar_mensajes mt-2"><i class="fa fa-search"></i></button>
-                                        </div>
-                                   </div>-->
-
                                     <div id="chatbox">
                                         <?php 
                                             $usuario = $_SESSION['usuario'][0]["id"];
